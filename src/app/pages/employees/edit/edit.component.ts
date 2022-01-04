637e62bf69dab4940848e250d337f08540376c28
Lizeth-Del-Rio <liz-delrio@outlook.com>
2022-01-04 15:28:15 -0500

    finalizacion formulario reactivo

--- src/app/pages/employees/edit/edit.component.ts
+++ src/app/pages/employees/edit/edit.component.ts
@@ -12,6 +12,7 @@ export class EditComponent implements OnInit {
   value: any = null
   employeeForm!: FormGroup
   private isEmail = /\S+@\S+\.\S+/
+  private isNumber = /^\d+$/
 
   constructor(private router: Router, private fb: FormBuilder) {
     const navigation = this.router.getCurrentNavigation()
@@ -28,11 +29,18 @@ export class EditComponent implements OnInit {
       lastName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.pattern(this.isEmail)]],
       startDate: ['', [Validators.required]],
-      phone: ['', [Validators.required]]
+      phone: ['', [Validators.required, Validators.pattern(this.isNumber)]]
     })
   }
 
+  get name() {return this.employeeForm.get('name')}
+  get lastName() {return this.employeeForm.get('lastName')}
+  get email() {return this.employeeForm.get('email')}
+  get startDate() {return this.employeeForm.get('startDate')}
+  get phone() {return this.employeeForm.get('phone')}
+
   onSave(): void{
+    alert('save')
     console.log('saved', this.employeeForm.value)
   }
 

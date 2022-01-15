98be0ed2962ef0a80b8de705f9c2daa5eaf4f76e
Lizeth-Del-Rio <liz-delrio@outlook.com>
2022-01-14 23:34:44 -0500

    se afinan detalles en las rutas

--- src/app/pages/employees/list/list-routing.module.ts
+++ src/app/pages/employees/list/list-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list.component';
 
-const routes: Routes = [{ path: '', component: ListComponent }];
+const routes: Routes = [
+  { path: '',
+    component: ListComponent,
+    children: [
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: 'list', component: ListComponent }
+    ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
